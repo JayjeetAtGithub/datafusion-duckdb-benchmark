b6c4e5532fcff76c5081d2d703d5e33cb76beb01
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-08-08 10:02:14 -0700

    Run datafusion on multiple cores

--- run-datafusion.sh
+++ run-datafusion.sh
@@ -13,7 +13,7 @@ echo "$DATAFUSION_CLI" >> result.csv
 echo "$CREATE" >> result.csv
 echo `date` >> result.csv
 echo "**********" >> result.csv
-echo "Query,iteration,time" >> result.csv
+echo "Query,parallelism,iteration,time" >> result.csv
 
 cat queries-datafusion.sql | while read query; do
     sync
@@ -24,19 +24,24 @@ cat queries-datafusion.sql | while read query; do
     echo "$query"
 
     echo -n "["
-    for i in $(seq 1 $TRIES); do
-		# 1. there will be two query result, one for creating table another for executing the select statement
-		# 2. each query contains a "Query took xxx seconds", we just grep these 2 lines
-		# 3. use sed to take the second line
-		# 4. use awk to take the number we want
-		RES=`${DATAFUSION_CLI} -f ${CREATE} /tmp/query.sql 2>&1 | grep "Query took" | sed -n 2p | awk '{print $7}'`
-		[[ $RES != "" ]] && \
-			echo -n "$RES" || \
-			echo -n "null"
-        [[ "$i" != $TRIES ]] && echo -n ", "
 
-	echo "${QUERY_NUM},${i},${RES}" >> result.csv
+    for c in 1 2 4 8; do
+        export DATAFUSION_EXECUTION_TARGET_PATITIONS=$c
+        for i in $(seq 1 $TRIES); do
+            # 1. there will be two query result, one for creating table another for executing the select statement
+            # 2. each query contains a "Query took xxx seconds", we just grep these 2 lines
+            # 3. use sed to take the second line
+            # 4. use awk to take the number we want
+            RES=`${DATAFUSION_CLI} -f ${CREATE} /tmp/query.sql 2>&1 | grep "Query took" | sed -n 2p | awk '{print $7}'`
+            [[ $RES != "" ]] && \
+                echo -n "$RES" || \
+                echo -n "null"
+            [[ "$i" != $TRIES ]] && echo -n ", "
+
+        echo "${QUERY_NUM},${c},${i},${RES}" >> result.csv
+        done
     done
+
     echo "],"
 
     QUERY_NUM=$((QUERY_NUM + 1))
