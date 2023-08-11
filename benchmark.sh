4806e1ebb3d4b9fbb29999801af5429ebf2a94a1
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-08-11 09:39:29 -0700

    Update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,10 @@
 # Runs several benchmark
 
-DATAFUSION_CLI=./datafusion-cli.28.0.0 CREATE=create-single-datafusion.sql bash run-datafusion.sh
+# DATAFUSION_CLI=./datafusion-cli.28.0.0 CREATE=create-single-datafusion.sql bash run-datafusion.sh
 CREATE=create-single-duckdb.sql bash run-duckdb.sh
 
 # note need to run this with q34 commented out
-DATAFUSION_CLI=./datafusion-cli.27.0.0 CREATE=create-single-datafusion.sql bash run-datafusion.sh
+# DATAFUSION_CLI=./datafusion-cli.27.0.0 CREATE=create-single-datafusion.sql bash run-datafusion.sh
 
 ## This is multi file
 ## DataFusion fails due to https://github.com/apache/arrow-datafusion/issues/7039
@@ -12,3 +12,10 @@ DATAFUSION_CLI=./datafusion-cli.27.0.0 CREATE=create-single-datafusion.sql bash 
 #CREATE=create-multi-duckdb.sql  bash run-duckdb.sh
 #DATAFUSION_CLI=./datafusion-cli.28.0.0 CREATE=create-multi-datafusion.sql  bash run-datafusion.sh
 #DATAFUSION_CLI=./datafusion-cli.27.0.0 CREATE=create-multi-datafusion.sql  bash run-datafusion.sh
+
+
+rm datafusion.csv
+rm duckdb.csv
+
+CREATE=create-multi-datafusion.sql  bash run-datafusion.sh
+CREATE=create-multi-duckdb.sql  bash run-duckdb.sh
