48de1dee2f07bc88c3cf4c666051392e7a5ec821
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-08-13 22:39:57 -0700

    Add a debug script

---
+++ debug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+CREATE=${CREATE:-create-single-datafusion.sql}
+DATAFUSION_CLI=${DATAFUSION_CLI:-datafusion-cli}
+
+cat queries-datafusion.sql | while read query; do
+    sync
+    echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
+    sync
+
+    echo "$query" > /tmp/query.sql
+    echo "qnum: $QUERY_NUM"
+    echo "$query"
+
+    ${DATAFUSION_CLI} -f ${CREATE} /tmp/query.sql
+done
