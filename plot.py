b6e3cb6711e3d91d12ee29ba9346c98888b11d84
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-08-14 15:27:48 -0700

    Adjustments

--- plot.py
+++ plot.py
@@ -5,32 +5,36 @@ import seaborn as sns
 import sys
 
 if __name__ == "__main__":
-    db = str(sys.argv[1])
     sns.set_theme(style="whitegrid")
-    data = {
-        "query_no": list(),
-        "cores": list(),
-        "duration": list()   
-    }
+    dbs = ["datafusion_main", "duckdb_main"]
 
-    with open(f'{db}.csv') as f:
-        lines = f.readlines()
+    for db in dbs:
+        data = {
+            "query_no": list(),
+            "cores": list(),
+            "duration": list()   
+        }
+        with open(f'experiments/v1/{db}.csv') as f:
+            lines = f.readlines()
 
-    for line in lines:
-        line = line.strip()
-        line = line.split(',')
-        query_no = int(line[0])
-        cores = int(line[1])
-        iteration = int(line[2])
-        duration = float(line[3])
+        for line in lines:
+            line = line.strip()
+            line = line.split(',')
+            query_no = int(line[0])
+            cores = int(line[1])
+            iteration = int(line[2])
+            duration = float(line[3])
 
-        data["query_no"].append(query_no)
-        data["cores"].append(cores)
-        data["duration"].append(duration)
+            data["query_no"].append(query_no)
+            data["cores"].append(cores)
+            data["duration"].append(duration)
 
-    df = pd.DataFrame(data)
-    sns.barplot(x="query_no", errwidth=1, capsize=0.1, errorbar="sd", y="duration", hue="cores", data=df)
-    plt.xlabel("Query No.")
-    plt.ylabel("Duration (s)")
-    plt.title(f"{db} query scaling")
-    plt.show()
+        df = pd.DataFrame(data)
+        sns.barplot(x="query_no", errwidth=1, capsize=0.1, errorbar="sd", y="duration", hue="cores", data=df)
+        plt.xlabel("Query No.")
+        plt.ylabel("Duration (s)")
+        plt.title(f"{db} query scaling")
+        plt.tight_layout()
+        plt.show()
+        plt.clf()
+        plt.cla()
