d82882a16c84dafe6d7c33ff0b3ef61ad78a0bab
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-08-22 16:37:15 -0700

    Remove the files before

--- clickbench/benchmark.sh
+++ clickbench/benchmark.sh
@@ -4,8 +4,8 @@ set -ex
 mode=$1 # single/multi
 sweep_cores=$2 # single/multi
 
-rm -rf datafusion.csv
-rm -rf duckdb.csv
+rm -rf clickbench_datafusion.csv
+rm -rf clickbench_duckdb.csv
 
 CREATE=create-$mode-duckdb.sql  bash run-duckdb.sh $sweep_cores
 CREATE=create-$mode-datafusion.sql  bash run-datafusion.sh $sweep_cores
