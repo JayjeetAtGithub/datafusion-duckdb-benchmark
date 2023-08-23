85b94b6c818b692b1d948dc2a22f4c289011bc40
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-08-22 18:34:45 -0700

    Fix tpch df script

--- tpch/run-datafusion.sh
+++ tpch/run-datafusion.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-PWD=$(pwd)
+pwd=$(pwd)
 cd ../arrow-datafusion
 git checkout 29.0.0
 cd benchmarks
 
 ./bench.sh data tpch
 ./bench.sh run tpch
-cd ${PWD}
+cd ${pwd}
 
 cp ../arrow-datafusion/benchmarks/results/HEAD/tpch.json tpch_datafusion.json
