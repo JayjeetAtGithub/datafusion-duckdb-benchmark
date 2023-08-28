ff18246c8fb58e046e36634a7d773fe0c4fdb4b2
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-08-28 14:14:11 -0700

    Read from datafusion queries

--- clickbench/debug.sh
+++ clickbench/debug.sh
@@ -3,7 +3,7 @@ set -e
 
 CREATE=${CREATE:-create-multi-datafusion.sql}
 
-cat debug-queries.sql | while read query; do
+cat queries-datafusion.sql | while read query; do
     echo "$query" > /tmp/query.sql
     echo "$query"
     datafusion-cli -f ${CREATE} /tmp/query.sql
