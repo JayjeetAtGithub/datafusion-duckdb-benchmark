58bc536697515c128ffae6f47fa2e0014cc7696a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-19 12:06:03 -0700

    Fix create queries

--- tpch/create-single-datafusion.sql
+++ tpch/create-single-datafusion.sql
@@ -2,32 +2,32 @@
 
 CREATE EXTERNAL TABLE customer
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/customer';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/customer/part-0.parquet';
 
 CREATE EXTERNAL TABLE orders
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/orders';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/orders/part-0.parquet';
 
 CREATE EXTERNAL TABLE lineitem
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/nation';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/nation/part-0.parquet';
 
 CREATE EXTERNAL TABLE part
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/part';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/part/part-0.parquet';
 
 CREATE EXTERNAL TABLE partsupp
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/partsupp';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/partsupp/part-0.parquet';
 
 CREATE EXTERNAL TABLE region
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/region';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/region/part-0.parquet';
 
 CREATE EXTERNAL TABLE supplier
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/supplier';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/supplier/part-0.parquet';
 
 CREATE EXTERNAL TABLE nation
 STORED AS PARQUET
-LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/nation';
+LOCATION '../arrow-datafusion/benchmarks/data/tpch_1/nation/part-0.parquet';
