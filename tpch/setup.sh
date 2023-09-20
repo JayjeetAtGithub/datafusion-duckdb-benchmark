888710f3c0c5bbfb0027e1a04807a61e2440a123
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-20 12:18:55 -0700

    Generate tpch10

--- tpch/setup.sh
+++ tpch/setup.sh
@@ -4,4 +4,14 @@ set -e
 pwd=$(pwd)
 cd ../arrow-datafusion
 git checkout 31.0.0
-./benchmarks/bench.sh data tpch
+
+scale_factor=$1
+
+if [ "$scale_factor" == 1 ]; then
+    ./benchmarks/bench.sh data tpch
+elif [ "$scale_factor" == 10 ]; then
+    ./benchmarks/bench.sh data tpch10
+else 
+    echo "Invalid scale factor: $scale_factor"
+    exit 1
+fi
