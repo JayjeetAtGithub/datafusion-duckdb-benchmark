f48a35a47bf86002262cf41276591bc0d47443eb
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-20 12:44:10 -0700

    Update queries

--- tpch/create-single-duckdb.sql
+++ tpch/create-single-duckdb.sql
@@ -1,25 +1,25 @@
 -- Single parquet file
 
 CREATE VIEW customer AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/customer/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/customer/part-0.parquet');
 
 CREATE VIEW orders AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/orders/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/orders/part-0.parquet');
 
 CREATE VIEW lineitem AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/lineitem/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/lineitem/part-0.parquet');
 
 CREATE VIEW part AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/part/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/part/part-0.parquet');
 
 CREATE VIEW partsupp AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/partsupp/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/partsupp/part-0.parquet');
 
 CREATE VIEW region AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/region/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/region/part-0.parquet');
 
 CREATE VIEW supplier AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/supplier/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/supplier/part-0.parquet');
 
 CREATE VIEW nation AS
-SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_sf1/nation/part-0.parquet');
+SELECT * FROM read_parquet('../arrow-datafusion/benchmarks/data/tpch_dataset/nation/part-0.parquet');
