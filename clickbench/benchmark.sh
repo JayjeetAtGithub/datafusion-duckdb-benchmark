96fe379ea32950c9d5b6fc78ae552e1b29ee332c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-26 09:22:50 -0700

    Fix typo

--- clickbench/benchmark.sh
+++ clickbench/benchmark.sh
@@ -7,5 +7,5 @@ sweep_cores="${2:-single}" # single/multi
 rm -rf clickbench_datafusion.csv
 rm -rf clickbench_duckdb.csv
 
-bash ../commom/run-datafusion.sh create-$mode-datafusion.sql queries-datafusion.sql $sweep_cores clickbench_datafusion.csv
-bash ../commom/run-duckdb.sh create-$mode-duckdb.sql queries-duckdb.sql $sweep_cores clickbench_duckdb.csv
+bash ../common/run-datafusion.sh create-$mode-datafusion.sql queries-datafusion.sql $sweep_cores clickbench_datafusion.csv
+bash ../common/run-duckdb.sh create-$mode-duckdb.sql queries-duckdb.sql $sweep_cores clickbench_duckdb.csv
--- tpch/benchmark.sh
+++ tpch/benchmark.sh
@@ -6,5 +6,5 @@ sweep_cores="${1:-single}" # single/multi
 rm -rf tpch_datafusion.csv 
 rm -rf tpch_duckdb.csv
 
-bash ../commom/run-datafusion.sh create-single-datafusion.sql queries.sql $sweep_cores tpch_datafusion.csv
-bash ../commom/run-duckdb.sh create-single-duckdb.sql queries.sql $sweep_cores tpch_duckdb.csv
+bash ../common/run-datafusion.sh create-single-datafusion.sql queries.sql $sweep_cores tpch_datafusion.csv
+bash ../common/run-duckdb.sh create-single-duckdb.sql queries.sql $sweep_cores tpch_duckdb.csv
