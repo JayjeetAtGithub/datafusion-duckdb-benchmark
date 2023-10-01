4d1fdc5ae8e7351887d59c2fcfdfb6793ea29811
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-01 00:30:57 -0700

    Fix core scaling in datafusion

--- common/run-query-datafusion.py
+++ common/run-query-datafusion.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     for c in cores:
         # create a DataFusion context
         config = SessionConfig().with_target_partitions(c)
-        ctx = SessionContext(config, RuntimeConfig)
+        ctx = SessionContext(config, RuntimeConfig())
 
         with open(create_query_file, "r") as f:
             create_queries = f.readlines()
