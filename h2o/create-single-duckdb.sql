c4f538d471f820ab03c81b868db569f5873f5759
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-14 20:25:50 -0700

    Add create view queries for duckdb

--- h2o/create-single-duckdb.sql
+++ h2o/create-single-duckdb.sql
@@ -1,2 +1,5 @@
-CREATE VIEW h2o_group AS
-SELECT * FROM read_csv_auto('G1_1e7_1e2_5_0.csv');
+CREATE VIEW h2o_group AS SELECT * FROM read_csv_auto('G1_1e7_1e2_5_0.csv');
+CREATE VIEW h2o_join AS SELECT * FROM read_csv_auto('J1_1e7_NA_0_0.csv.gz');
+CREATE VIEW h2o_join_small AS SELECT * FROM read_csv_auto('J1_1e7_1e1_0_0.csv.gz');
+CREATE VIEW h2o_join_medium AS SELECT * FROM read_csv_auto('J1_1e7_1e4_0_0.csv.gz');
+CREATE VIEW h2o_join_big AS SELECT * FROM read_csv_auto('J1_1e7_1e7_0_0.csv.gz');
