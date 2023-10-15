5ec512512cd78b2485953be9f0813ab17d328b49
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-14 20:22:30 -0700

    Download dataset for joins in h2o

--- h2o/setup.sh
+++ h2o/setup.sh
@@ -6,3 +6,19 @@ set -e
 rm -rf G1_1e7_1e2_5_0.csv*
 wget https://github.com/duckdb/duckdb-data/releases/download/v1.0/G1_1e7_1e2_5_0.csv.gz
 gunzip G1_1e7_1e2_5_0.csv.gz
+
+rm -rf J1_1e7_NA_0_0.csv.gz
+wget https://github.com/duckdb/duckdb-data/releases/download/v1.0/J1_1e7_NA_0_0.csv.gz
+gunzip J1_1e7_NA_0_0.csv.gz
+
+rm -rf J1_1e7_1e1_0_0.csv.gz
+wget https://github.com/duckdb/duckdb-data/releases/download/v1.0/J1_1e7_1e1_0_0.csv.gz
+gunzip J1_1e7_1e1_0_0.csv.gz
+
+rm -rf J1_1e7_1e4_0_0.csv.gz
+wget https://github.com/duckdb/duckdb-data/releases/download/v1.0/J1_1e7_1e4_0_0.csv.gz
+gunzip J1_1e7_1e4_0_0.csv.gz
+
+rm -rf J1_1e7_1e7_0_0.csv.gz
+wget https://github.com/duckdb/duckdb-data/releases/download/v1.0/J1_1e7_1e7_0_0.csv.gz
+gunzip J1_1e7_1e7_0_0.csv.gz
