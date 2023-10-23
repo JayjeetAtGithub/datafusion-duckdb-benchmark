f9dbd055e636df7a15bcf4bb0c7499defa260983
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-22 18:56:10 -0700

    Use 5GB files for h2o groupby benchmark

--- h2o/create-single-duckdb.sql
+++ h2o/create-single-duckdb.sql
@@ -1,4 +1,4 @@
-CREATE VIEW x_group AS SELECT * FROM read_csv_auto('G1_1e7_1e2_5_0.csv');
+CREATE VIEW x_group AS SELECT * FROM read_csv_auto('5GB.csv');
 CREATE VIEW x AS SELECT * FROM read_csv_auto('J1_1e7_NA_0_0.csv');
 CREATE VIEW small AS SELECT * FROM read_csv_auto('J1_1e7_1e1_0_0.csv');
 CREATE VIEW medium AS SELECT * FROM read_csv_auto('J1_1e7_1e4_0_0.csv');
